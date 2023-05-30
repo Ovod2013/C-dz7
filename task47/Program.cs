b28ad6e393fc3c8f9027246049baa41056db8598
Ovod2013 <Ovod2013@yandex.ru>
2023-05-30 16:43:36 +0300

    задача 47  разбираюсь

---
+++ task47/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(m, n, 10, 110);// Вызов метода GetArray 
+// и передача ему количества столбцов, строк, максим и миним границ чисел
+PrintArray(array);
+
+
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    Random random = new Random();
+    double x = 0d;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            //result[i, j] = new Random().Next(minValue, maxValue)/10;
+
+            x = random.NextDouble();
+            if (x<0.1) Console.WriteLine(x*1);
+            result[i, j] = 0;
+            //Console.Write(result[i, j]);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j] * 100000} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ task4701/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double y;
+y=1.23;
+
+Console.WriteLine(y);
+
+
+int amount = 4562122;
+//Convert.ToInt32(Console.ReadLine());
+            double[] massiv = new double[amount];
+ 
+            
+            double x = 0d;
+            Random random = new Random();
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                massiv[i] = random.NextDouble();
+                x = massiv[i];
+                if (x<0.0001) Console.WriteLine(x*1000000);
+            }
+ 
+            //Console.Write(Array.Sort(x));
